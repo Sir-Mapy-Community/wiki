a96f3eaf6edf8d63b3dbb5b6ed53edcc61e3d383
Samuel Ogu <hello@samuelogu.com>
2022-04-02 17:48:23 +0100

    app routes update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'how-to-buy',
+    loadChildren: () => import('./pages/how-to-buy/how-to-buy.module').then(m => m.HowToBuyModule)
+  },
+  { path: '**',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
