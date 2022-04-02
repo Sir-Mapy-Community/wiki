a57620a6dab4afa5187629e0035a4d8bf22695a2
Samuel Ogu <hello@samuelogu.com>
2022-04-02 18:33:45 +0100

    working route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/how-to-buy/how-to-buy-routing.module.ts
+++ src/app/pages/how-to-buy/how-to-buy-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HowToBuyComponent} from "./how-to-buy.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HowToBuyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
