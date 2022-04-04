bda244caefa549ce2b9572a0bd3d55f830ab60d7
Samuel Ogu <hello@samuelogu.com>
2022-04-04 08:04:02 +0100

    features

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,8 @@ export const environment = {
   facebook: 'https://www.facebook.com/wikicatbsc',
   contract: '0x6Ec90334d89dBdc89E08A133271be3d104128Edb',
   chart: 'https://poocoin.app/tokens/0x6ec90334d89dbdc89e08a133271be3d104128edb',
-  pancakeswap: 'https://pancakeswap.finance/swap?outputCurrency=0x6ec90334d89dbdc89e08a133271be3d104128edb'
+  pancakeSwap: 'https://pancakeswap.finance/swap?outputCurrency=0x6ec90334d89dbdc89e08a133271be3d104128edb',
+  LockedLiquidity: 'https://app.unicrypt.network/amm/pancake-v2/pair/0x933477eba23726cA95A957cB85dBB1957267EF85'
 };
 
 /*
